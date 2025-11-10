99f7092608bea70fbc7039a1aba465882c67bb5b
root <root>
2025-11-10 18:04:51 +0530

    First Working

--- learn_fastapi/main.py
+++ learn_fastapi/main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from learn-fastapi!")
+from fastapi import FastAPI #Import
+from typing import Optional
+
+app=FastAPI() #Instance
 
 
-if __name__ == "__main__":
-    main()
+#Make note all dynamic routing must be below and arreng
+# them properly and also make sure if you have static rout
+# which is related to dynamic one or prefix
+# match with static route so you have to arreng it properly
+
+# Queary peremter
+# http://localhost:5010/about?limit=100
+
+@app.get('/about')
+def data(limit=10, published:bool = True, sort:Optional[str] = None):
+    if published:
+        return {'data':f'{limit} published blogs from db'}
+    else:
+        return {'data':f'{limit} blogs from db'}
+
+@app.get('/about/{id}')
+def id(id:int):
+    if int(id)==100:
+        return (f"HI Abdul your id is {id}")
+    else:
+        return (f"your id {id} not found")
+
+@app.get('/') #Decorator
+def index(): #Function
+    return {'data':{'name':'abdul'}}
+
